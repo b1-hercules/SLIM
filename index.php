9007b74f91830aefdeeb363c74566e19f3e345c4
iqbal115705 <sugandhiiqbal@egmail.com>
2018-04-08 10:35:03 +0700

    twig templating

--- index.php
+++ index.php
@@ -6,15 +6,27 @@ $app = new Slim\App;
 
 $container = $app->getContainer();
 
-$container['hello'] = function()
-{
-  echo "Bung!!!";
-};
+// $container['hello'] = function()
+// {
+//   echo "Bung!!!";
+// };
+// Register component on container
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig( __DIR__ . '/templates', [
+        'cache' => false
+    ]);
 
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $container['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($container['router'], $basePath));
+
+    return $view;
+};
 
 $app->get('/', function ($request, $response)
 {
-  $this->hello;
+  return $this->view->render($response, 'home.twig ');
+  // $this->hello;
 });
 
 //{
