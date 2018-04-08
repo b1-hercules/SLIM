464dca7f7773ca08ab4d4ab0717c1fd1f8684e24
iqbal115705 <sugandhiiqbal@egmail.com>
2018-04-08 08:27:39 +0700

    request&response get methode

--- index.php
+++ index.php
@@ -5,18 +5,18 @@ require __DIR__ . '/vendor/autoload.php';
 $app = new Slim\App;
 $app->get('/', function ($request, $response)
 {
-  return 'Halo Semuaaaanyaa!!!';
+  return $request->getMethod();
 });
 
-$app->get('/forum[/{title}]', function ($request, $response, $args)
-{
-  //die(var_dump($args));
-  if (empty($args)) {
-    echo "ini halaman index forum";
-  }else {
-    return $args['title'];
-  }
-});
+// $app->get('/forum[/{title}]', function ($request, $response, $args)
+// {
+//   //die(var_dump($args));
+//   if (empty($args)) {
+//     echo "ini halaman index forum";
+//   }else {
+//     return $args['title'];
+//   }
+// });
 
 $app->run();
 ?>
