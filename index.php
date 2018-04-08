085c0056cc295cc602080e1c938aa17cc2777d0e
iqbal115705 <sugandhiiqbal@egmail.com>
2018-04-08 08:14:56 +0700

    titile opsional

--- index.php
+++ index.php
@@ -8,10 +8,14 @@ $app->get('/', function ($request, $response)
   return 'Halo Semuaaaanyaa!!!';
 });
 
-$app->get('/forum/{title}/{user}', function ($request, $response, $args)
+$app->get('/forum[/{title}]', function ($request, $response, $args)
 {
   //die(var_dump($args));
-  return $args['title'].' ditulis oleh '.$args['user'];
+  if (empty($args)) {
+    echo "ini halaman index forum";
+  }else {
+    return $args['title'];
+  }
 });
 
 $app->run();
