16d809a65a8ec8ce3a4b80b4727f615fdf0fdcfc
iqbal115705 <sugandhiiqbal@egmail.com>
2018-04-08 07:02:56 +0700

    prettyUrl.flexTittle.dump$args

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+require __DIR__ . '/vendor/autoload.php';
+$app = new Slim\App;
+$app->get('/', function ($request, $response)
+{
+  return 'Halo Semuaaaanyaa!!!';
+});
+
+$app->get('/forum/{title}', function ($request, $response, $args)
+{
+  die(var_dump($args));
+  return $args;
+});
+
+$app->run();
+?>
