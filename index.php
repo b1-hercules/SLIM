976a3b304e8a28bd3e5e35416d8e12a072aae75e
iqbal115705 <sugandhiiqbal@egmail.com>
2018-04-08 08:06:56 +0700

    flex title 2 dengan user

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $app->get('/', function ($request, $response)
   return 'Halo Semuaaaanyaa!!!';
 });
 
-$app->get('/forum/{title}', function ($request, $response, $args)
+$app->get('/forum/{title}/{user}', function ($request, $response, $args)
 {
   //die(var_dump($args));
-  return $args['title'];
+  return $args['title'].' ditulis oleh '.$args['user'];
 });
 
 $app->run();
